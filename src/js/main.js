babc6af59ee267d9eff2102e02115c4cf328de2d
Diana <dianaga93@hotmail.com>
2024-04-27 00:44:26 +0200

    fetch and render

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-console.log('>> Ready :)');
+const btnSearch = document.querySelector('.js-btn-search');
+const input = document.querySelector('.js-input');
+const ulList = document.querySelector('.js-ul');
+
+let drinks = [];
+
+const render = (result) => {
+  for (const eachdrink of result.drinks) {
+    ulList.innerHTML += `<li>
+    <img src="${eachdrink.strDrinkThumb}" alt="">
+    <h3>${eachdrink.strDrink}</h3>
+    </li>`   
+  }
+}
+
+
+function handleSearch (ev){
+    ev.preventDefault();
+    const search = input.value;
+
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${search}`)
+    .then(response => response.json())
+    .then((result) => {
+        console.log(result.drinks);
+        render(result);
+       
+    })
+   
+}
+
+
+
+btnSearch.addEventListener('click', handleSearch);
+
